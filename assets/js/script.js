ebc9ee1dbcc2cc86476af77014e43fe2010f0c52
M.Naveed <naveedghulamfareed22@gmail.com>
2024-12-06 17:08:55 +0500

    minor change

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,19 +23,77 @@ document.addEventListener("DOMContentLoaded", () => {
     duration: 1,
     scrollTrigger: {
       trigger: ".third-row-col",
-      // start: "center center", // Start when the center of `.third-row` reaches the center of the viewport
       end: " center",
     },
   });
-  gsap.from(".last-container-about", {
+  gsap.from(".blog-content-animation-class", {
     opacity: 0,
     y: 250,
     duration: 1,
     scrollTrigger: {
-      trigger: ".last-container-about",
-      // start: "center center", // Start when the center of `.third-row` reaches the center of the viewport
-      end: "center",
+      trigger: ".blog-content-animation-class",
+      end: "start",
+    },
+  });
+  gsap.from(".service-fifth-row-col", {
+    opacity: 0,
+    y: 250,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".service-fifth-row-col",
+      end: "start",
+    },
+  });
+  gsap.from(".fourth-service-row-animation", {
+    opacity: 0,
+    y: 250,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".fourth-service-row-animation",
+      end: "start",
+    },
+  });
+  gsap.from(".service-second-row-col", {
+    opacity: 0,
+    y: 250,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".service-second-row-col",
+      end: "start",
     },
   });
 
+
+
+});
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".about-page-main-colom .service-first-heading", {
+  opacity: 0,
+  y: 200,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".about-page-main-colom",
+    start: "top 80%",
+  },
+});
+
+gsap.from(".about-page-main-colom .service-first-para", {
+  opacity: 0,
+  y: 200,
+  duration: 1.2,
+  scrollTrigger: {
+    trigger: ".about-page-main-colom",
+    start: "top 80%",
+  },
+});
+
+gsap.from(".about-page-main-colom .service-first-row-image", {
+  opacity: 0,
+  y: 200,
+  duration: 1.4,
+  scrollTrigger: {
+    trigger: ".about-page-main-colom",
+    start: "top 80%",
+  },
 });
