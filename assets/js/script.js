a48bc2418ae54e9b79c57337a6dca3afeeacdb83
M.Naveed <naveedghulamfareed22@gmail.com>
2024-12-09 17:04:57 +0500

    origin main

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,12 +17,250 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
+  gsap.from(".animate-heading-project-section", {
+
+    opacity: 0,
+    y: 20,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".animate-heading-project-section",
+      start: "top bottom",
+    
+    },
+  });
+  gsap.from(".animate-img-project-section", {
+    opacity: 0,
+    y: 60,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".animate-img-project-section",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".img-slider1", {
+    opacity: 0,
+    x: -50,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".img-slider1",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".img-slider3", {
+    opacity: 0,
+    x: 50,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".img-slider3",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".img-slider2", {
+    opacity: 0,
+    y: 90,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".img-slider3",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".animate-plan-index-interior", {
+    opacity: 0,
+    x: -70,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".animate-plan-index-interior",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".animate-plan-index-exterior", {
+    opacity: 0,
+    x: 70,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".animate-plan-index-interior",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".animate-plan-index-center", {
+    opacity: 0,
+    y: 70,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".animate-plan-index-interior",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".animate-slider-index-counter", {
+    opacity: 0,
+    y: 30,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".img-slider3",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".animate-icon-coial-index", {
+    opacity: 0,
+    x: -30,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".img-slider3",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".index-latest-project-animation-heading", {
+    opacity: 0,
+    x: -100,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".index-latest-project-animation-heading",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".index-latest-project-animation-img", {
+    opacity: 0,
+    x: 250,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".index-latest-project-animation-heading",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".animate-heading1-first-section-index", {
+    opacity: 0,
+    y: 70,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".img-slider3",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".animate-heading1-second-section-index", {
+    opacity: 0,
+    y: 70,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".animate-heading1-second-section-index",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".animate-button-second-section-index", {
+    opacity: 0,
+    y: 40,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".animate-heading1-second-section-index",
+      start: "top bottom",
+    },
+  });
+  gsap.from(".animate-heading-project-section2", {
+
+    opacity: 0,
+    y: 20,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".animate-heading-project-section2",
+      start: "top bottom",
+    
+    },
+  });
+  gsap.from(".animate-img-project-section2", {
+    opacity: 0,
+    y: 60,
+    duration: 1,
+    delay:0.1,
+    scrollTrigger: {
+      trigger: ".animate-img-project-section2",
+      start: "top bottom",
+    },
+  });
+
   gsap.from(".third-row-col", {
     opacity: 0,
     y: 150,
     duration: 1,
     scrollTrigger: {
       trigger: ".third-row-col",
+      end: " center",
+    },
+  });
+  gsap.from(".index-howWork-headig-animate", {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".index-howWork-headig-animate",
+      end: " center",
+    },
+  });
+  gsap.from(".index-howWork-card1-animate", {
+    opacity: 0,
+    x: -100,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".index-howWork-headig-animate",
+      end: " center",
+    },
+  });
+  gsap.from(".index-howWork-card2-animate", {
+    opacity: 0,
+    y: 100,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".index-howWork-headig-animate",
+      end: " center",
+    },
+  });
+  gsap.from(".index-howWork-card3-animate", {
+    opacity: 0,
+    x: 100,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".index-howWork-headig-animate",
+      end: " center",
+    },
+  });
+  gsap.from(".index-unique-img1-animate", {
+    opacity: 0,
+    x: -150,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".index-unique-img1-animate",
+      end: " center",
+    },
+  });
+  gsap.from(".index-unique-img2-animate", {
+    opacity: 0,
+    x: 150,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".index-unique-img1-animate",
+      end: " center",
+    },
+  });
+  gsap.from(".index-unique-heading-animate", {
+    opacity: 0,
+    y: 150,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".index-unique-img1-animate",
       end: " center",
     },
   });
